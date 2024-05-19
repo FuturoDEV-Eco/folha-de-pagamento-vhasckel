f240bc49582eeb84c98284240d79f7921be3395a
vanessa hasckel <vanessahasckel00@hotmail.com>
2024-05-18 23:13:35 -0300

    resolução questão 04

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 const { calcularIR } = require("./calculo_imposto_renda");
 const { calcularINSS } = require("./calculo_inss");
+const { calcularSalarioLiquido } = require("./calculo_salario_liquido");
 
 console.log(calcularINSS(2409));
-console.log(calcularIR(10000));
+console.log(calcularIR(2409));
+console.log(calcularSalarioLiquido(2409));
